45cd1ecbe7767028c7a958d3fc0c1f7712723721
Daniel <dacjnr22@gmail.com>
2022-04-20 20:14:29 +0200

    UserInterfaceDesign w/Backend

---
+++ app/src/main/java/com/four_meals_dining/User.java
@@ -0,0 +1,13 @@
+package com.four_meals_dining;
+
+public class User {
+
+    private String name, surname, email, password;
+
+    public User(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+}
