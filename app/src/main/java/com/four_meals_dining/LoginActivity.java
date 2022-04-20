48cb4c25e132d041cba9b946750533ef56c12c54
Daniel <dacjnr22@gmail.com>
2022-04-20 20:14:15 +0200

    UserInterfaceDesign w/Backend

--- app/src/main/java/com/four_meals_dining/LoginActivity.java
+++ app/src/main/java/com/four_meals_dining/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.four_meals_dining;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,17 +10,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     ImageView logo;
     TextView TV_Login,TV_DHAC;
     TextInputLayout til_username, til_password;
-    TextInputEditText tiet_username,tiet_password;
     Button login;
+
+    FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -35,10 +43,14 @@ public class LoginActivity extends AppCompatActivity {
         til_username=  findViewById(R.id.til_username);
         til_password=  findViewById(R.id.til_password);
 
-        tiet_username= findViewById(R.id.tiet_username);
-        tiet_password = findViewById(R.id.tiet_password);
-
         login= findViewById(R.id.btn_Login);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
 
         TV_DHAC.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,9 +63,44 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openHomeActivity= new Intent(LoginActivity.this, AdminHomeActivity.class);
-                startActivity(openHomeActivity);
+
+                String Username= til_username.getEditText().getText().toString().trim();
+                String Password= til_password.getEditText().getText().toString().trim();
+                loginUser(Username, Password);
+
             }
         });
+    }
+
+    private void loginUser(String username, String password) {
+
+        firebaseAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            //userOrAdminLogin(username);
+                            Toast.makeText(getApplicationContext(), "Welcome Admin", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Password or Username Wrong!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void userOrAdminLogin(String username) {
+
+        if(username.contains("fourmealsapp.com")){
+            Toast.makeText(getApplicationContext(), "Welcome Admin", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(LoginActivity.this, AdminHomeActivity.class));
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+        }else{
+            Toast.makeText(getApplicationContext(), "User Login Success", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+        }
     }
 }
--- app/src/main/java/com/four_meals_dining/SignupActivity.java
+++ app/src/main/java/com/four_meals_dining/SignupActivity.java
@@ -1,28 +1,47 @@
 package com.four_meals_dining;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
     TextView signup, policy;
     TextInputLayout til_fName, til_surname, til_su_username, til_su_password, til_con_password;
-    TextInputEditText tiet_fName, tiet_surname, tiet_su_username, tiet_su_password, tiet_con_password;
     Button submit;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        firebaseAuth= FirebaseAuth.getInstance();
         signup= findViewById(R.id.TVSignup);
         policy= findViewById(R.id.tv_signup);
 
@@ -32,21 +51,156 @@ public class SignupActivity extends AppCompatActivity {
         til_su_password= findViewById(R.id.til_su_password);
         til_con_password= findViewById(R.id.til_conpassword);
 
-        tiet_fName= findViewById(R.id.tiet_fname);
-        tiet_surname= findViewById(R.id.tiet_surname);
-        tiet_su_username= findViewById(R.id.tiet_su_username);
-        tiet_su_password= findViewById(R.id.tiet_su_password);
-        tiet_con_password= findViewById(R.id.tiet_conpassword);
-
         submit= findViewById(R.id.btn_Signup);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent openMenu = new Intent(SignupActivity.this, MainActivity.class);
-                startActivity(openMenu);
+
+                String FirstName= til_fName.getEditText().getText().toString().trim();
+                String Surname= til_surname.getEditText().getText().toString().trim();
+                String Username= til_su_username.getEditText().getText().toString().trim();
+                String Password= til_su_password.getEditText().getText().toString().trim();
+
+                if(valFullName() && valSurname() && valUsername() && valPassword() && valPasswordMatch()){
+
+                    firebaseAuth.createUserWithEmailAndPassword(Username, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                               newUserSign(FirstName, Surname, Username, Password);
+
+                                Toast.makeText(getApplicationContext(), "Signup Successful", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            }else{
+                                Toast.makeText(getApplicationContext(), "Signup Failed. Try again.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }else{
+                    Toast.makeText(SignupActivity.this, "Signup Error", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
 
+    public boolean valFullName(){
+
+        String firstName= til_fName.getEditText().getText().toString().trim();
+        String checkSpace= "\\A\\w{1,20}\\z";
+
+        if(firstName.isEmpty()){
+            til_fName.setError("Required");
+            return false;
+        }else if(!firstName.matches(checkSpace)){
+            til_fName.setError("Remove whitespaces");
+            return false;
+        }
+        else{
+            til_fName.setError(null);
+            til_fName.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public boolean valSurname(){
+
+        String surname= til_surname.getEditText().getText().toString().trim();
+        String checkSpace= "\\A\\w{1,20}\\z";
+
+        if(surname.isEmpty()){
+            til_surname.setError("Required");
+            return false;
+        }else if(!surname.matches(checkSpace)){
+            til_surname.setError("Remove whitespaces");
+            return false;
+        }
+        else{
+            til_surname.setError(null);
+            til_surname.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public boolean valUsername(){
+
+        String username= til_su_username.getEditText().getText().toString().trim();
+        String checkSpace= "^(.+)@(.+)$";
+
+        if(username.isEmpty()){
+            til_su_username.setError("Required");
+            return false;
+        }else if(!username.matches(checkSpace)){
+            til_su_username.setError("Remove whitespaces");
+            return false;
+        }
+        else{
+            til_su_username.setError(null);
+            til_su_username.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public boolean valPassword() {
+
+        Pattern pattern;
+        Matcher matcher;
+
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        String password = til_su_password.getEditText().getText().toString().trim();
+
+        if (!password.matches(PASSWORD_PATTERN)) {
+            til_su_password.setError("Invalid Password");
+            return false;
+        } else {
+            return true;
+
+        }
+    }
+    public boolean valPasswordMatch(){
+
+        String password= til_su_password.getEditText().getText().toString().trim();
+        String password2= til_con_password.getEditText().getText().toString().trim();
+
+        if(password.isEmpty()){
+            til_con_password.setError("Required");
+            return false;
+        }else if(!password2.equals(password)){
+            til_con_password.setError("Passwords do not match");
+            return false;
+        }
+        else{
+            til_con_password.setError(null);
+            til_con_password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void newUserSign(String fName, String surname, String username, String password ){
+
+        db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("First Name", fName);
+        user.put("Surname", surname);
+        user.put("Username", username);
+        user.put("Password", password);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(),
+                                "DocumentSnapshot added with ID: " + documentReference.getId(),
+                                Toast.LENGTH_LONG).
+                                show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error adding document" + e,
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
     }
 }
